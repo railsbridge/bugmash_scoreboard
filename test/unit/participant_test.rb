cbab83b26d7d9677b93c494954a9a87d9bcba949
Mike Breen <hardbap@gmail.com>
2009-07-28 12:39:57 -0400

    remove dummy test from participant.

--- test/unit/participant_test.rb
+++ test/unit/participant_test.rb
@@ -1,8 +1,5 @@
-require 'test_helper'
+require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class ParticipantTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+
 end
