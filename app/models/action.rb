b184ece57b29bc1695de62e2616d1de3027a7f2e
Mike Breen <hardbap@gmail.com>
2009-07-31 12:51:10 -0400

    add some validations to the Action model.

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,3 +1,6 @@
 class Action < ActiveRecord::Base
   belongs_to :participant
+
+  validates_presence_of :lighthouse_id
+  validates_numericality_of [:lighthouse_id, :point_value]
 end
--- test/unit/action_test.rb
+++ test/unit/action_test.rb
@@ -2,4 +2,7 @@ require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class ActionTest < ActiveSupport::TestCase
   should_belong_to :participant
+
+  should_validate_presence_of :lighthouse_id
+  should_validate_numericality_of :lighthouse_id, :point_value
 end
