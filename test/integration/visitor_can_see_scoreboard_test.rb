1ff3acfa37b20aa966a52cf88f2c188e8968e6d1
Mike Breen <hardbap@gmail.com>
2009-08-04 09:29:01 -0400

    Use "visit" in tests.

--- test/integration/visitor_can_see_scoreboard_test.rb
+++ test/integration/visitor_can_see_scoreboard_test.rb
@@ -5,12 +5,12 @@ class VisitorCanSeeScoreboardTest < ActionController::IntegrationTest
     setup { @participant = Factory(:participant) }
 
     should 'see a list of Participants' do
-      visits root_path
+      visit root_path
       assert_contain 'John McClane'
     end
 
     should "be able to see Participant's Actions" do
-      visits root_path
+      visit root_path
       click_link 'John McClane'
       assert_equal participant_path(@participant), path
     end
