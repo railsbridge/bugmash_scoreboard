a016eba682fe83a6e67d7c0702cc4566d29b9f9d
Mike Breen <hardbap@gmail.com>
2009-08-05 15:41:45 -0400

    Admin can create a new contribution.

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,8 +1,23 @@
 class ContributionsController < ApplicationController
   before_filter :login_required
-
+  
+  def index
+    @contributions = Contribution.all(:order => 'updated_at DESC')
+  end
+  
   def new
     @contribution = Contribution.new
     @participants = Participant.all(:select => 'id, name')
+  end
+
+  def create
+    @contribution = Contribution.new(params[:contribution])
+    
+    if @contribution.save
+      flash[:notice] = 'Contribution created.'
+      redirect_to contributions_path
+    else
+      render :new
+    end
   end
 end
