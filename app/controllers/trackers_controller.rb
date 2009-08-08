c8e440d86eca7954199f266acf043b6bcf858a96
Mike Breen <hardbap@gmail.com>
2009-08-08 13:53:31 -0400

    I want to see all tracked items.

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -2,6 +2,6 @@ class TrackersController < ApplicationController
   before_filter :login_required
 
   def index
-    @trackers = Tracker.not_processed
+    @trackers = Tracker.all.paginate(:order => 'created_at DESC', :page => params[:page], :per_page => 25)
   end
 end
