ddc873a7b43b438dee4e81cfae19b766189b4384
Mike Breen <hardbap@gmail.com>
2009-08-08 12:25:53 -0400

    lets add a route for Trackers.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :sessions, :only => [:new, :create, :destroy]
   map.resources :contributions
   map.resources :issues, :except => [:show, :destroy]
+  map.resources :trackers, :only => :index
   map.signin '/signin', :controller => 'sessions', :action => 'new'
   map.signout '/signout', :controller => 'sessions', :action => 'destroy'
   map.root :controller => 'participants', :action => 'index'
