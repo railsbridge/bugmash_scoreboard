5154667188296fa7bd0ac028e526e143a975b6a2
Mike Breen <hardbap@gmail.com>
2009-08-11 09:15:27 -0400

    shut off Issues; change table caption

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :participants
   map.resources :sessions, :only => [:new, :create, :destroy]
   map.resources :contributions
-  map.resources :issues, :except => [:show, :destroy]
+#  map.resources :issues, :except => [:show, :destroy]
   map.resources :trackers, :only => :index
   map.signin '/signin', :controller => 'sessions', :action => 'new'
   map.signout '/signout', :controller => 'sessions', :action => 'destroy'
